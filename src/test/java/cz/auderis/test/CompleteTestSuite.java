4a81645d598e7b8b1afc7c67a7004dcd3638a4a3
Boleslav Bobcik <bbobcik@gmail.com>
2016-11-13 00:59:31 +0100

    Aggregating test suites added

---
+++ src/test/java/cz/auderis/test/CompleteTestSuite.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Boleslav Bobcik - Auderis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cz.auderis.test;
+
+import cz.auderis.test.logging.LoggingTestSuite;
+import cz.auderis.test.matcher.MatcherTestSuite;
+import cz.auderis.test.parameter.annotation.ParamAnnotationTestSuite;
+import cz.auderis.test.rule.RuleTestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        LoggingTestSuite.class,
+        MatcherTestSuite.class,
+        ParamAnnotationTestSuite.class,
+        RuleTestSuite.class
+})
+public class CompleteTestSuite {
+
+    // Only a suite definition
+
+}
